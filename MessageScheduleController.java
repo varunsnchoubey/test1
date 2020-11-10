263c842ea4132beac21162d197347b865c21b7d2
Varun Choubey <varun.choubey1989@gmail.com>
2020-11-10 09:40:03 -0500

    Adding project
    uploading project

---
+++ MessageScheduleController.java
@@ -0,0 +1,30 @@
+package com.messagescheduler.controller;
+
+import java.text.ParseException;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.messagescheduler.model.MessageInfo;
+import com.messagescheduler.service.MessageSchedulerServiceImpl;
+
+@RestController
+public class MessageScheduleController {
+
+	@Autowired
+	MessageSchedulerServiceImpl service;
+
+	@PostMapping("/message")
+	public ResponseEntity<Object> scheduleMessage(@Valid @RequestBody MessageInfo messageInfo) throws ParseException {
+		// persist and schedule message task
+		service.persistAndScheduleMesssageTask(messageInfo);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+
+}
